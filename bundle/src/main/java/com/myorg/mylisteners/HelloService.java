e679cb8bec90ca820f2b254d0fd4025c9872867f
unknown <aaysriva@aaysriva-W7.corp.adobe.com>
2016-08-30 18:44:01 +0530

    listens for event NODE_ADDED

---
+++ bundle/src/main/java/com/myorg/mylisteners/HelloService.java
@@ -0,0 +1,13 @@
+package com.myorg.mylisteners;
+
+/**
+ * A simple service interface
+ */
+public interface HelloService {
+    
+    /**
+     * @return the name of the underlying JCR repository implementation
+     */
+    public String getRepositoryName();
+
+}
